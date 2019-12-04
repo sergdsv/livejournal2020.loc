e954a5e17df4ebe2c9341f163e3c0a31c931fe25
Serg DSV <serg.dsv@gmail.com>
2019-12-04 13:55:34 +0000

    add testUnit and Comments.vue

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(ArticlesTableSeeder::class);
-         $this->call(CommentsTableSeeder::class);
+//         $this->call(ArticlesTableSeeder::class);
+//         $this->call(CommentsTableSeeder::class);
 
     }
 }
